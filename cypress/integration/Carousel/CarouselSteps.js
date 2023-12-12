19775168901e97f89198acf35e8f8ce2b004f01f
Julian Riedinger <julianriedinger27@gmail.com>
2023-12-12 15:51:18 -0300

    changed step structure

--- cypress/integration/Carousel/CarouselSteps.js
+++ cypress/integration/Carousel/CarouselSteps.js
@@ -8,15 +8,15 @@ Given('I visit the home page', () => {
   home.visit()
 })
 
+When('I interact with the featured items carousel', () => {
+  home.nextCarouselItem()
+})
+
 Then('I should see at least one featured item displayed', () => {
   home.getCarouselItems()
     .should("be.visible")
   home.getCarouselItems()
     .should("have.length.greaterThan", 1)
-})
-
-When('I interact with the featured items carousel', () => {
-  home.nextCarouselItem()
 })
 
 Then('I should be able to navigate through different featured items', () => {
